99d3d3e6e12700338bd4cf089e1bb36abec5ae14
Ron Grass <GrassRo@brc.gbl>
2017-08-01 14:12:05 -0500

    Cleaned up comments in app.js

--- js/app.js
+++ js/app.js
@@ -1,45 +1,33 @@
-
+// Create difficulty parameter to use as a speed multiplier. 100 is default but later we can utilize this to let the user select a difficulty for the game.
 var difficulty = 100;
 
-// Enemies our player must avoid
+// Instantiate new enemies, set their initial x, y positions, sprite and speed multiplier
 var Enemy = function(enemyrow) {
-    // Variables applied to each of our instances go here,
     this.sprite = 'images/enemy-bug.png';
-    //RON COMMENTS: Enemy class called to instantiate (create new) enemies, set their initial x, y positions and speed multiplier
-    this.x = -101;  // Change to -101 to start off screen.
-    this.y = enemyrow * 83; // set row position base on passed enemynumber.
-    this.speed = random(1,5); // randomly set the speed to 1 - 5. Will use this with dt and difficulty param to change position
+    this.x = -101;  // start off screen.
+    this.y = enemyrow * 83; // set row position based on passed enemynumber.
+    this.speed = random(1,5); // randomly set the speed to 1-5. Will use this with dt and difficulty param to change position
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-//RON COMMENT increment position using randomly picked speed, difficulty level (default 100) and dt param.
+// Update the enemy's position using randomly picked speed, difficulty level and dt param.
 Enemy.prototype.update = function(dt) {
     this.x = this.x + (this.speed * dt * difficulty);
-   // console.log(this.y + this.x);
-// RON COMMENT: if the enemy is offscreen, replace that enemy in the array with a new enemy created with the enemy class.
+// if the enemy is offscreen, replace that enemy in the array with a new enemy object
     if (this.x > 505) {
-       // allEnemies[(this.y/83)-1] = new Enemy(this.y/83);
-       // Benny's improvement
         allEnemies[allEnemies.indexOf(this)] = new Enemy(this.y/83);
     };
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-//RON comment: Building out skeleton of player class
-
+// Player class
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.x = 202  // Change to -101 to start off screen.
-    this.y =  5 * 83; // set row position base on passed enemynumber.
+    this.x = 202;  // Set starting x postion to middle column
+    this.y = 415; // set starting y position to bottom of screen
 };
 
 Player.prototype.update = function(dt) {
@@ -50,6 +38,7 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(key) {
+// For each keystroke, check if movement will take the player offscreen. If not, move that direction.
     switch(key) {
         case "left":
             if (player.x != 0) {
@@ -72,35 +61,19 @@ Player.prototype.handleInput = function(key) {
             };
             break;
     };
-        // 37: 'left',
-        // 38: 'up',
-        // 39: 'right',
-        // 40: 'down'
-        console.log(key);
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// RON COMMENTS: Video only shows three enemies at once. Thought to:
-// - create allenemies array.
-// - define three enemies with initial row, board position and speeds.
-// - Reuse the same enemies (array only ever has three values) CONFIRMED engine.js uses FOR EACH to parse array
-// - Check each enemies' position, if offscreen randomly select its new row and speed. New position always starts off screen lef of its random row
-
-//RON COMMENT: Build enemies using Enemy class (contructor). Creats each enemy and assigns variables.
+// Create array to hold enemy objects. Add three enemies using enemy class and passing parameter to set their initial row position.
 var allEnemies = [
     new Enemy(1),
     new Enemy(2),
     new Enemy(3),
 ];
 
-
 // Place the player object in a variable called player
-//RON COMMENT: Added player var using Player() class. Just skeleton
 var player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and send to Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -112,10 +85,7 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// RON COMMENT: Created random row / column function for enemy positioning
-// Called when creating initial position of enemies
-// Called when...
-
+// Random function to support enemy speed and other needs. Accepts mim/max values.
 function random(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
